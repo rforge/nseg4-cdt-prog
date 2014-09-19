d695116916f572806541e7d13d74f8c95893b02e
sarahheaps <sarahheaps@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-09-19 16:16:53 +0000

    Added source.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nseg4-cdt-prog@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/cdtRprogramming-package.R
@@ -0,0 +1,11 @@
+#' R courses for Newcastle University's CDT in Cloud 
+#' Computing for Big Data
+#' 
+#' Functions and datasets used for Newcastle University's 
+#' CDT in Cloud Computing for Big Data. This package is 
+#' used in the "R Programming" course.
+#' @name cdtRprogramming-package
+#' @aliases cdtRprogramming
+#' @docType package
+#' @keywords package
+NULL
